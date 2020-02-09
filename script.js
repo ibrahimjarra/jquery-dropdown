f1657e438c2a58598516e15b9185c6426507ba3a
ibrahim jarra <39721918+ibrahimj14@users.noreply.github.com>
2020-02-08 22:08:56 -0500

    refactor with if statment

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-$(document).ready(function() {
-  $(".dictionary").hide();
 
+$(document).ready(function() {
+  var hide = $(".dictionary").hide();
+  var open = true;
   $("#button").click(function() {
-    $(".drop_down").css("align-items", "flex-start");
-    $("#dropDown").height("300px");
-    $(".dictionary").show();
-    $("#button").click(function() {
-      $("#dropDown").height ("auto");
-      $(".dictionary").hide();
-    });
+    if (open) {
+      $(".drop_down").css("align-items", "flex-start");
+      $("#dropDown").height("300px");
+      var show = $(".dictionary").show();
+      open = false;
+    } 
+    else if (!open) {
+      $("#dropDown").height("auto");
+      hide = $(".dictionary").hide();
+    } else {
+        pass;
+    };
   });
 });
+
+/*
+
+var hide = $(".dictionary").hide();
+  $("#button").click(function() {
+    if (hide) {
+      $(".drop_down").css("align-items", "flex-start");
+      $("#dropDown").height("300px");
+      $(".dictionary").show();
+    };
+    else {
+      $("#dropDown").height("auto");
+      $(".dictionary").hide();
+    };
+  });
+
+  */
